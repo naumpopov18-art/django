f9d202f3e552f956171b352f9af0b12313f85935
naumpopov18-art <naum.popov@mail.ru>
2026-03-04 05:44:54 +0500

    Update views.py

--- dj-homeworks/2.1-databases/work_with_database/phones/views.py
+++ dj-homeworks/2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from phone.models import Phone
 
 
 def index(request):
@@ -7,11 +8,17 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = get_object_or_404(Phone, slug=slug)
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
