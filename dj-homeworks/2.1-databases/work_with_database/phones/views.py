cc6c05837d710039793762927274af85ca3294ed
naumpopov18-art <naum.popov@mail.ru>
2026-03-07 14:16:08 +0500

    Update views.py

--- dj-homeworks/2.1-databases/work_with_database/phones/views.py
+++ dj-homeworks/2.1-databases/work_with_database/phones/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from phone.models import Phone
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
-    template = 'catalog.html'
-    phones = Phone.objects.all()
-    context = {
-        'phones': phones
-    }
-    return render(request, template, context)
-
+    sort = request.GET.get('sort')
+    
+    if sort == 'name':
+        phones = Phone.objects.order_by('name')
+    elif sort == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif sort == 'max_price':
+        phones = Phone.objects.order_by('-price')
+    else:
+        phones = Phone.objects.all()
+    
+    return render(request, 'catalog.html', {'phones': phones})
 
 def show_product(request, slug):
     template = 'product.html'
+    
     phone = get_object_or_404(Phone, slug=slug)
+    
     context = {
         'phone': phone
     }
